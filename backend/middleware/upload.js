5951b57ff61bb3c6a011e88c0ff4462c46b8e949
Brijesh kumar <139457145+1234bk@users.noreply.github.com>
2025-08-04 19:11:25 +0530

    Update upload.js

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,19 +1,7 @@
 // middleware/upload.js
 const multer = require('multer');
-const path = require('path');
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-   const fullPath = path.join(__dirname, '..', 'uploads');
-  console.log('Saving uploaded image to:', fullPath);
-  cb(null, fullPath);
-  },
-  filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    const uniqueName = `${Date.now()}-${Math.round(Math.random() * 1e9)}${ext}`;
-    cb(null, uniqueName);
-  },
-});
+const storage = multer.memoryStorage();
 
 const fileFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) cb(null, true);
@@ -23,7 +11,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 },
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
 });
 
 module.exports = upload;
