5ae5ec9f0c55d9708ecd879906cd118815c1af62
Brijesh kumar <139457145+1234bk@users.noreply.github.com>
2025-08-04 21:16:51 +0530

    Update EditProfile.jsx

--- frontend/src/components/EditProfile.jsx
+++ frontend/src/components/EditProfile.jsx
@@ -57,6 +57,12 @@ const EditProfile = () => {
 
   return (
     <div className="max-w-2xl mx-auto p-8 mt-10 bg-white shadow-lg rounded-lg animate-fade-in border border-blue-200">
+      <button
+                onClick={() => navigate(-1)}
+                className="ml-6 mb-5 mt-6 px-4 py-2 bg-blue-100 text-blue-700 rounded hover:bg-blue-200 transition"
+            >
+                ← Back
+            </button>
       <h2 className="text-2xl font-bold text-blue-700 mb-6 text-center">Edit Profile</h2>
 
       <form onSubmit={handleSubmit} className="space-y-5">
